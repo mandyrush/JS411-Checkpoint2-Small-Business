dbf1dffa53f86eeb8de7ca25efad0bd46f651df4
mandyrush <mandyrush85@gmail.com>
2021-12-16 19:39:42 -0700

    create login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,36 @@
 import React from "react";
 
 import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 const Login = () => {
     return (
-        <Container maxWidth="lg" sx={{ my: '3rem' }}>
-            Login
-        </Container>
+        <Container maxWidth="sm" sx={{ my: '3rem' }}>
+            <TextField
+                fullWidth
+                hiddenLabel
+                required
+                id="username"
+                label="Username"
+                defaultValue=""
+                variant="standard"
+            />
+            <TextField
+                fullWidth
+                hiddenLabel
+                id="password"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                variant="standard"
+            />
+            <Button variant="contained" sx={{
+                mt: 3, backgroundColor: '#E0E0E0', color: '#000', '&:hover': {
+                    backgroundColor: '#CCC',
+                }
+            }} fullWidth>Login</Button>
+        </Container >
     );
 }
 
